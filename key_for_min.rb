3941bda462f8d4e9eb373d60af5cffa34df7e6cf
Tommy Werner <wtwerner@gmail.com>
2020-05-14 04:06:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,10 +4,12 @@
 def key_for_min_value(name_hash)
   name_hash.each do |nm, val|
     i = 0
+    min_val = []
     if val == i
-     name_hash[nm]
+      min_val << nm
     else
      i += 1
    end
+   min_val[0]
  end
 end
